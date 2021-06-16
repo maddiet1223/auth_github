d14316e5cf2b16f0e87a8e6d287fab3621cb30ff
Madhavi Thurlapati <madhavi.thurlapati@revature.net>
2021-06-16 14:26:36 -0700

    authentication

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,8 +1,13 @@
+// const { GraphQLID } = require("graphql");
 const graphql = require("graphql");
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLID } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
+  fields: {
+    //just to avoid error because every new graphql object type should have atleast one field
+    dummyField: { type: GraphQLID },
+  },
 });
 
 module.exports = RootQueryType;
